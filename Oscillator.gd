508470200f60d26aef9dc1c979626a511a9d873b
relser <relser99@gmail.com>
2021-01-09 20:17:35 -0700

    Finished basic scanning minigame.

--- Oscillator.gd
+++ Oscillator.gd
@@ -1,6 +1,6 @@
 extends Control
 
-export var maxScale = 5.0
+export var maxSize = 1.0
 export var targetT = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -9,4 +9,4 @@ func _ready():
 
 
 func _process(delta):
-	self.rect_scale.x = maxScale * targetT
+	self.rect_size.x = maxSize * targetT
