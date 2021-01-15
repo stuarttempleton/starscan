8d618c2c989f1de90743e58deeba1d95eb07a50c
stuarttempleton <bvrn007@gmail.com>
2021-01-14 21:13:45 -0700

    enter system button hooked up

--- scripts/SysInfo_scan_button.gd
+++ scripts/SysInfo_scan_button.gd
@@ -1,5 +1,7 @@
 extends Button
 
+export var scene_to_load = ""
+export var additive_load_scene = true #additive or replacement
 
 func _ready():
 	connect("mouse_entered",self,"mouse_enter")
@@ -7,7 +9,12 @@ func _ready():
 	connect("pressed", self, "on_button_pressed")
 
 func on_button_pressed () :
-	print("button pressed")
+	print("Load: %s" % [scene_to_load])
+	if additive_load_scene:
+		var loaded_scene = load(scene_to_load)
+		add_child(loaded_scene.instance())
+	else:
+		get_tree().change_scene(scene_to_load)
 
 func mouse_enter():
 	GameController.EnableDisableMovement(false)
