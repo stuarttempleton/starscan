60e2e4ea09682088a74404b2b4727ff9fdea677a
relser <relser99@gmail.com>
2021-01-15 23:24:13 -0700

    Display scan result in system info.

--- scripts/SystemInfoController.gd
+++ scripts/SystemInfoController.gd
@@ -7,7 +7,7 @@ var DisplayedSystem
 func _ready():
 	$Background.connect("mouse_entered",self,"mouse_enter")
 	$Background.connect("mouse_exited",self,"mouse_exit")
-	$Background/InfoContainer/ScanButton.connect("minigameComplete", self, "RefreshScannability")
+	$Background/InfoContainer/ScanButton.connect("minigameComplete", self, "RefreshDisplayedData")
 
 func mouse_enter():
 	GameController.EnableDisableMovement(false)
@@ -16,12 +16,12 @@ func mouse_exit():
 	GameController.EnableDisableMovement(true)
 
 func ScanTextHelper(scan):
-	if scan:
-		return "Yes"
+	if scan < 0.001:
+		return "Unknown"
 	else:
-		return "No"
+		return str(int(scan * 10) * 10) + "% identified"
 
-func OutpustTextHelper(system):
+func OutpostTextHelper(system):
 	if StarMapData.SystemHasOutpost(system):
 		return "Yes"
 	else:
@@ -31,12 +31,21 @@ func InRange(system):
 	$Background.visible = true
 	if DisplayedSystem != system:
 		DisplayedSystem = system
-		$Background/InfoContainer/SystemName.text = DisplayedSystem.Name
-		$Background/InfoContainer/SystemDetail.text = system_detail_boilerplate % [OutpustTextHelper(DisplayedSystem), DisplayedSystem.Planets.size(), ScanTextHelper(DisplayedSystem.Scan), "Unknown"]
-		RefreshScannability()
+		RefreshDisplayedData()
 
 func NotInRange():
 	$Background.visible = false
+	
+func RefreshDisplayedData():
+	RefreshSystemNameText()
+	RefreshDetailText()
+	RefreshScannability()
+	
+func RefreshSystemNameText():
+	$Background/InfoContainer/SystemName.text = DisplayedSystem.Name
+	
+func RefreshDetailText():
+	$Background/InfoContainer/SystemDetail.text = system_detail_boilerplate % [OutpostTextHelper(DisplayedSystem), DisplayedSystem.Planets.size(), ScanTextHelper(DisplayedSystem.Scan), "Unknown"]
 	
 func RefreshScannability():
 	$Background/InfoContainer/ScanButton.disabled = (DisplayedSystem.Scan > 0)
