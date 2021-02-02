a3062cbd4f77e544d5198f26b1622e7606dac12a
stuarttempleton <bvrn007@gmail.com>
2021-02-02 12:29:36 -0700

    background star field emitters using our star gfx

--- scripts/Starfield.gd
+++ scripts/Starfield.gd
@@ -11,8 +11,8 @@ func _ready():
 
 func _draw():
 	#draw space background
-	draw_rect( Rect2(Vector2(-5,-5), Vector2(MapScale + 10,MapScale + 10)), Color(0.289063, 0.289063, 0.289063) ) 
-	draw_rect( Rect2(Vector2(0,0), Vector2(MapScale,MapScale)), Color(0,0,0) )
+	#draw_rect( Rect2(Vector2(-5,-5), Vector2(MapScale + 10,MapScale + 10)), Color(0.289063, 0.289063, 0.289063) ) 
+	#draw_rect( Rect2(Vector2(0,0), Vector2(MapScale,MapScale)), Color(0,0,0) )
 	 
 	var systems = StarMapData.Systems()
 	for system in systems :
