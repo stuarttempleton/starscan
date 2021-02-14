9b0d49f7af17d554fb98778e10e663710abc703e
Stuart Templeton <bvrn007@gmail.com>
2021-02-14 13:56:23 -0700

    tweaking the game first moments for the player.

--- scripts/StarMapViewport.gd
+++ scripts/StarMapViewport.gd
@@ -2,7 +2,23 @@ extends Node2D
 
 func _ready():
 	ShipData.connect("FuelTanksEmpty", self, "_on_FuelTanksEmpty")
+	if ShipData.Ship().FirstRun:
+		_on_FirstPlay()
+
+func _on_FirstPlay():
+	#place at center
+	ShipData.Ship().X = 0.5
+	ShipData.Ship().Y = 0.5
 	
+	#deplete fuel to force education about refueling
+	ShipData.ConsumeFuel(ShipData.Ship().Fuel)
+	
+	#display start up dialog "greetings, nomad!"
+	
+	#add that you've completed the first moments of the game
+	ShipData.Ship().FirstRun = false
+
+
 func _on_FuelTanksEmpty():
 	var shipPos = Vector2(ShipData.Ship().X,ShipData.Ship().Y)
 	var nearestOutpostSystem = StarMapData.GetNearestOutpostSystem(shipPos)
