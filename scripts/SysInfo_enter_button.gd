8c2ceedf97bbb1842e91c677e411b15bde64662a
Stuart Templeton <bvrn007@gmail.com>
2021-03-01 15:38:13 -0700

    bug fix: enter system button had click-drift

--- scripts/SysInfo_enter_button.gd
+++ scripts/SysInfo_enter_button.gd
@@ -1,6 +1,24 @@
 extends Button
 
 export(String) var SystemViewport_scene
+var hoverflag = false
+
+func _ready():
+	connect("mouse_entered",self,"mouse_enter")
+	connect("mouse_exited",self,"mouse_exit")
+
+
+func mouse_enter():
+	print("mouse enter...")
+	hoverflag = true
+	GameController.EnableMovement(false)
+
+
+func mouse_exit():
+	if hoverflag:
+		GameController.EnableMovement(true)
+
 
 func _on_button_down():
+	hoverflag = false
 	get_tree().change_scene(SystemViewport_scene)
