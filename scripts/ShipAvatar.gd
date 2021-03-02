821bdd96ec3920545b7a208482e7a5fc44be7b7c
Stuart Templeton <bvrn007@gmail.com>
2021-03-02 13:29:58 -0700

    acceleration/deceleration is implemented. kinda.

--- scripts/ShipAvatar.gd
+++ scripts/ShipAvatar.gd
@@ -4,12 +4,14 @@ export (NodePath) var TargetDebug
 
 var target = Vector2()
 var velocity = Vector2()
+var CurrentSpeed = 0
 var MapScale = StarMapData.MapScale
 var MinCameraZoom = 0.5
 var MaxCameraZoom = 2
 var mouseIsPressed = false
 
 func _ready():
+	print("AccelerationRate: ", ShipData.Ship().AccelerationRate)
 	self.position = Vector2(ShipData.Ship().X * MapScale, ShipData.Ship().Y * MapScale)
 	target = self.position
 	GameController.EnableMovement(true)
@@ -29,7 +31,27 @@ func _input(event):
 
 func Zoom(amount) :
 	$Camera2D.zoom = Vector2(clamp($Camera2D.zoom.x + amount, MinCameraZoom, MaxCameraZoom), clamp($Camera2D.zoom.y + amount, MinCameraZoom, MaxCameraZoom) )
-	
+
+
+func HandleThrusters():
+	if CurrentSpeed < 0.01:
+		$Thruster1.emitting = false
+		$Thruster1/Thruster2.emitting = false
+		$Thruster2.emitting = false
+		$Thruster2/Thruster2.emitting = false
+	elif CurrentSpeed < ShipData.Ship().TravelSpeed / 2:
+		$Thruster1.emitting = true
+		$Thruster1/Thruster2.emitting = true
+		$Thruster2.emitting = false
+		$Thruster2/Thruster2.emitting = false
+	elif CurrentSpeed < ShipData.Ship().TravelSpeed:
+		$Thruster1.emitting = true
+		$Thruster1/Thruster2.emitting = true
+		$Thruster2.emitting = true
+		$Thruster2/Thruster2.emitting = true
+	pass
+
+
 func HandleBoundary() :
 	if target.x > MapScale :
 		target.x = MapScale
@@ -41,17 +63,30 @@ func HandleBoundary() :
 		target.y = 0
 		
 func _physics_process(_delta):
+	var ship = ShipData.Ship()
 	
 	if mouseIsPressed:
 		target = get_global_mouse_position()
+		#CurrentSpeed = min(CurrentSpeed + _delta * ship.AccelerationRate, ship.TravelSpeed)
 	
 	HandleBoundary()
+	HandleThrusters()
 	
-	var ship = ShipData.Ship()
-	var distanceToTravel = min(ship.TravelSpeed * _delta, position.distance_to(target))
+	var distanceToTarget = position.distance_to(target)
+	
+	if distanceToTarget > 0.001:
+		CurrentSpeed = min(CurrentSpeed + _delta * ship.AccelerationRate, ship.TravelSpeed)
+	
+	var distanceToTravel = min(CurrentSpeed * _delta, distanceToTarget)
+	var approachSpeed = ship.TravelSpeed * 0.3
+	var decelerate = (distanceToTarget - distanceToTravel < approachSpeed) and (CurrentSpeed > approachSpeed)
 	
 	if distanceToTravel > 0.001 && ship.Fuel > 0.001:
-		
+		if decelerate:
+			CurrentSpeed = max(CurrentSpeed - _delta * ship.AccelerationRate * distanceToTravel, approachSpeed)
+			print("DECEL")
+			print("distanceToTarget: ", distanceToTarget, ", ", distanceToTravel, ", ", CurrentSpeed)
+			
 		var fuelRequired = min(distanceToTravel * ship.FuelPerUnitDistance, ship.Fuel)
 		var availableFuelFraction = min(1.0, ship.Fuel / ship.FuelPerUnitDistance)
 		var speedToHitTargetThisFrame = availableFuelFraction * distanceToTravel / _delta
@@ -64,6 +99,7 @@ func _physics_process(_delta):
 	else:
 		
 		target = position
+		CurrentSpeed = 0
 	
 	TargetDebug.position = target
 	_setMapPosition(position)
