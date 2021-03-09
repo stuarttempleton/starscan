dba28e785ccab44074b2f644192a73dfac76c74e
Stuart Templeton <bvrn007@gmail.com>
2021-03-08 17:13:42 -0700

    grid overlay test

---
+++ scenes/Grid.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var MapScale = StarMapData.MapScale
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rect_position = Vector2( 0, 0 )
+	rect_size = Vector2(  MapScale,  MapScale )
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
