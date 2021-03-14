29e2dd58a06792d58d91d73b87c4ca21f2a77a82
relser <relser99@gmail.com>
2021-03-14 15:33:00 -0700

    Fuel and crew bars turn red below 20%.

--- scripts/ValueBarScale.gd
+++ scripts/ValueBarScale.gd
@@ -1,8 +1,10 @@
 extends ColorRect
 
-var lastpeek
 export(String) var ship_value_key
 export(String) var ship_capacity_key
+export(float) var warn_fraction
+export(Color) var normal_color
+export(Color) var warn_color
 
 func _ready():
 	print("%s %f, %s %f" % [ship_value_key, ShipData.StarShip[ship_value_key], ship_capacity_key, ShipData.StarShip[ship_capacity_key]])
@@ -11,6 +13,11 @@ func _ready():
 func SetPercent(fraction) :
 	lastpeek = fraction * ShipData.StarShip[ship_capacity_key]
 	rect_scale = Vector2(fraction, 1)
+	
+	if (fraction <= warn_fraction):
+		color = warn_color
+	else:
+		color = normal_color
 
 func _process(_delta):
 	if (ShipData.StarShip[ship_value_key] != lastpeek) :
