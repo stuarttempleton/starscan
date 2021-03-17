f576b9249bd935f8f31fb9793176bc45846361fc
relser <relser99@gmail.com>
2021-03-16 20:46:29 -0700

    Juice bars!

--- scripts/ValueBar.gd
+++ scripts/ValueBar.gd
@@ -7,38 +7,41 @@ export(Color) var normal_color
 export(Color) var warn_color
 export(float) var Lerp_Rate
 
+export(NodePath) var bg_animator_path
 export(NodePath) var scale_bar_path
 export(NodePath) var bar_particles_path
 export(NodePath) var fill_colorrect_path
 
-var scale_bar
-var bar_particles
-var fill_colorrect
+onready var bg_animator = get_node(bg_animator_path)
+onready var scale_bar = get_node(scale_bar_path)
+onready var bar_particles = get_node(bar_particles_path)
+onready var fill_colorrect = get_node(fill_colorrect_path)
 
 var targetScaleX
 var currentState = State.Normal
 
 func _ready():
 	print("%s %f, %s %f" % [ship_value_key, ShipData.StarShip[ship_value_key], ship_capacity_key, ShipData.StarShip[ship_capacity_key]])
-	print("scale bar path: %s" % scale_bar_path)
-	scale_bar = get_node(scale_bar_path)
-	bar_particles = get_node(bar_particles_path)
-	fill_colorrect = get_node(fill_colorrect_path)
-	DisplayFraction(GetModelFraction())
+	var startFraction = GetModelFraction()
+	SetPercent(startFraction)
+	DisplayFraction(startFraction)
+	HandleState()
 
 func _process(_delta):
 	Refresh()
 
 func Refresh():
 	SetPercent(GetModelFraction())
-	LerpToTarget()
+	EaseToTarget()
+	HandleState()
 		
-func LerpToTarget():
+func EaseToTarget():
 	var fractionDiff = scale_bar.rect_scale.x - targetScaleX
 	if (abs(fractionDiff) > 0.001) :
 		var fractionDelta = fractionDiff * Lerp_Rate
 		var newFraction = scale_bar.rect_scale.x - fractionDelta
 		DisplayFraction(newFraction)
+		bar_particles.set_emitting(fractionDiff > 0)
 		
 func GetModelFraction() -> float:
 	var value = ShipData.StarShip[ship_value_key]
@@ -52,24 +55,23 @@ func SetPercent(fraction) :
 	if (fraction > 1.0):
 		print("%s fraction %f is too high, clamping to 1.0" % [ship_value_key, fraction])
 		fraction = 1.0
-		
-#	if (fraction != targetScaleX):
-		#TODO: value just changed, trigger attention-grabbing effects?
 	targetScaleX = fraction
-	
-	HandleState()
 
 func HandleState():
 	for stateName in State:
 		var targetState = State[stateName]
 		if (currentState != targetState && CheckStateCondition(targetState)):
+			print("%s bar state condition met for state %s" % [ship_value_key, stateName])
 			ChangeState(targetState)
 			break
 		
-func CheckStateCondition(targetState) -> bool:		
+func CheckStateCondition(targetState) -> bool:
+	#print("%s bar targetScaleX=%f" % [ship_value_key, targetScaleX])
 	match targetState:
+		State.MegaWarn:
+			return targetScaleX <= 0.001
 		State.Warn:
-			return targetScaleX <= warn_fraction
+			return currentState != State.MegaWarn && targetScaleX <= warn_fraction
 		State.Normal:
 			return targetScaleX > warn_fraction
 		_:
@@ -83,8 +85,10 @@ func ChangeState(newState):
 	
 func DoStateExitWork(oldState):
 	match oldState:
+		State.MegaWarn:
+			bg_animator.stop()
 		State.Warn:
-			pass
+			bg_animator.stop()
 		State.Normal:
 			pass
 		_:
@@ -92,16 +96,19 @@ func DoStateExitWork(oldState):
 			
 func DoStateEnterWork(newState):
 	match newState:
+		State.MegaWarn:
+			fill_colorrect.color = warn_color
+			bg_animator.play("WarnFlash", -1, 2.0)
 		State.Warn:
 			fill_colorrect.color = warn_color
-			#TODO: play Warn fx
+			bg_animator.play("WarnFlash")
 		State.Normal:
 			fill_colorrect.color = normal_color
-			#TODO: play Normal fx
 		_:
 			print("State has no enter work: %s" % newState)
 
 enum State {
 	Normal,
+	MegaWarn,
 	Warn
 }
