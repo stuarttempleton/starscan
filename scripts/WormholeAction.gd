91f56d7494a2d82ff63ce7ec82e41899a9abcacf
Stuart Templeton <bvrn007@gmail.com>
2021-08-13 12:35:00 -0700

    support for audio sfx

--- scripts/WormholeAction.gd
+++ scripts/WormholeAction.gd
@@ -20,7 +20,7 @@ var min_lerp = 0.0
 
 var avatar
 var jump_location
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	avatar = get_parent()
 
@@ -29,9 +29,10 @@ func EnterWormholeTo(target):
 		jump_location = target
 		CurrentState = State.ScaleDown
 		avatar.look_enabled = false
+		AudioPlayer.PlaySFX(AudioPlayer.AUDIO_KEY.ANOMALY_TRAVEL)
 		SceneChanger.Fade(0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	match CurrentState:
 		State.ScaleDown:
@@ -41,8 +42,9 @@ func _process(delta):
 			if avatar.modulate.a == 0:
 				CurrentState = State.JumpLocation
 		State.JumpLocation:
-			avatar.JumpToMapPosition(jump_location) #or animate movement like a tow
+			avatar.JumpToMapPosition(jump_location)
 			CurrentState = State.ScaleUp
+			AudioPlayer.PlaySFX(AudioPlayer.AUDIO_KEY.ANOMALY_ARRIVE)
 			SceneChanger.UnFade(0)
 		State.ScaleUp:
 			avatar.modulate.a = clamp(avatar.modulate.a + delta * fade_rate, min_lerp, max_lerp)
@@ -50,7 +52,6 @@ func _process(delta):
 			avatar.scale.y = clamp(avatar.scale.y + delta * scale_rate, min_lerp, max_lerp)
 			if avatar.modulate.a == 1:
 				CurrentState = State.Nothing
-	if CurrentState == State.Nothing:
-		avatar.look_enabled = true
-	else:
+				avatar.look_enabled = true
+	if CurrentState != State.Nothing:
 		avatar.rotate(delta * rotation_rate * -1)
